f51e25c8fabb644cbb9b17bb530948d4796ee865
Dani <dani.93.perez@gmail.com>
2020-12-21 08:58:40 +0100

    Re-doing the cheat sheet. Added empy readme.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,5 +8,24 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void variablesConstants()
+        {
+            //Explicit declaration
+            byte byteVal = 2;
+            int intVal = 10;
+            float floatVal = 20.2f;
+            char charVal = 'a';
+            string stringVal = "Dani";
+            bool boolVal = false;
+
+            //Non-explicit declaration
+            var byteVar = 2;
+            var intVar = 10;
+            var floatVar = 20.2f;
+            var charVar = 'a';
+            var stringVar = "Dani";
+            var boolVar = false;
+        }
     }
 }
